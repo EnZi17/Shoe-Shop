03d02dc21993edb04666a49fb265c53517f3a1fd
EnZi <vominhthong117@gmail.com>
2024-09-24 19:01:52 +0700

    b

--- frontend/src/components/ProductDetail.js
+++ frontend/src/components/ProductDetail.js
@@ -8,7 +8,7 @@ function ProductDetail() {
   const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:9000/api/shoes/${id}`)
+    fetch(`https://shoe-shop-backend-qm9w.onrender.com/api/shoes/${id}`)
       .then(response => response.json())
       .then(data => setShoe(data))
       .catch(error => console.error('Error fetching shoe details:', error));
